0b85fe474388744285892ec0949f171ef2e1c7af
stevewangRMA <stevewangrma@gmail.com>
2026-06-15 01:23:25 -0400

    done

---
+++ while_loop_flag_version.py
@@ -0,0 +1,38 @@
+def get_starting_number():
+    while True:
+        response = input("How many bottles of beer on the wall? ")
+        if response.isnumeric():
+            number = int(response)
+            if number >= 1:
+                return number
+
+
+def sing(start):
+    n = start
+    keep_singing = True
+    while keep_singing:
+        if n == 1:
+            current = "1 bottle"
+        else:
+            current = str(n) + " bottles"
+
+        print(current + " of beer on the wall, " + current + " of beer.")
+
+        remaining = n - 1
+        if remaining == 0:
+            remaining_phrase = "no more bottles"
+        elif remaining == 1:
+            remaining_phrase = "1 bottle"
+        else:
+            remaining_phrase = str(remaining) + " bottles"
+
+        if n == 1:
+            print("Take it down, pass it around, " + remaining_phrase + " of beer on the wall!")
+        else:
+            print("Take one down, pass it around, " + remaining_phrase + " of beer on the wall.")
+
+        if n == 1:
+            keep_singing = False
+        else:
+            print()
+            n = n - 1
